e1d7c3270f520c5a4064654153a39021b8044f7e
HOMEE <howtostart33@gmail.com>
2026-07-30 03:19:46 +0600

    Update and rename README.md to App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RoutePlanner from './RoutePlanner.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RoutePlanner />
+    </div>
+  );
+}
+
+export default App;
